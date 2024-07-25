33383720a778b03274ad43f9c2c62606ba8ae341
paul <105162443+paulhwangj@users.noreply.github.com>
2024-07-24 21:03:46 -0500

    added two event listeners, one for when bot is online and when message that bot can read is sent

--- src/index.js
+++ src/index.js
@@ -8,12 +8,20 @@ dotenv.config();
 const client = new Client({
   intents: [
     // Intents = set of permissions that our bot can use in order to get access to a set of events
-    // Guild = a server
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
   ],
 });
+
+// event listener -- when bot is ready
+client.on('ready', (c) => {
+    console.log(`${c.user.username} is ready!`)
+});
+
+client.on('messageCreate', (message) => {
+    console.log(message)
+})
 
 client.login(process.env.LETTERBOTD_TOKEN);
